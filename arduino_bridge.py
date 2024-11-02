81f60b1031ac255fe9ade5c02b60850c139d0f71
Nick Ferguson <ndferguson@protonmail.com>
2024-11-02 17:03:52 -0400

    updates

--- arduino_bridge.py
+++ arduino_bridge.py
@@ -19,12 +19,26 @@ class ArduinoBridge(Node):
             self.listener_callback,
             10
         )
+
+        self.publisher_ = self.create_publisher(String, 'arduino_response', 10)
+
+        self.timer = self.create_timer(0.1, self.read_from_arduino)
+
         self.get_logger().info('Arduino Bridge Node Started')
 
     def listener_callback(self, msg):
         command = msg.data
         self.ser.write(command.encode())
         self.get_logger().info('Command sent to Arduino: "%s"' % command)
+
+    def read_from_arduino(self):
+        if self.ser.in_waiting > 0:
+            response = self.ser.readline().decode().strip()
+            if response:
+                self.get_logger().info('Data received from Arduino: "%s"' % response)
+                msg = String()
+                msg.data = response
+                self.publisher_.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
--- readme.md
+++ readme.md
@@ -2,3 +2,15 @@ A simple ros2 bridge to turn a LED on and off.
 
 https://github.com/Siddharth-Andhale/ROS2_Arduino_Bridge
 
+on the docker container in one terminal  
+`sudo docker run --rm -it --runtime nvidia --network host --gpus all --device=/dev/ttyACM0 -e DISPLAY dustynv/ros:humble-ros-base-l4t-r32.7.1`  
+`sudo docker cp arduino_bridge.py {docker_id}:/arduino_bridge.py`  
+`pip install pyserial`  
+`python arduino_bridge.py`
+
+in another terimal
+`sudo docker exec -it {docker_id} bash`
+`source /opt/ros/humble/install/setup.bash`
+`ros2 topic pub sketch_nov2a std_msgs/String '{data: "o"}'`
+`ros2 topic pub sketch_nov2a std_msgs/String '{data: "f"}'`
+
