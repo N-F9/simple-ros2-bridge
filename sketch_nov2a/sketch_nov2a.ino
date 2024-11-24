222c2ceff55813e30104d22f7370cceb73010542
Nick Ferguson <ndferguson@protonmail.com>
2024-11-24 11:03:48 -0500

    fixed code

--- sketch_nov2a/sketch_nov2a.ino
+++ sketch_nov2a/sketch_nov2a.ino
@@ -1,17 +1,18 @@
 const int ledPin = 13;
 char command;
+bool ledState = false;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin, LOW);
   Serial.begin(9600);
-  Serial.println("Type 'o' to turn on LED, 'f' to turn off LED");
+  Serial.println("Type 'o' to turn on LED, 'f' to turn off LED, 't' to toggle LED");
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    char temp = command;
     command = Serial.read();
+    Serial.print("Received command: ");
     Serial.println(command);
     
     if (command == 'o') { 
@@ -19,24 +20,27 @@ void loop() {
     } else if (command == 'f') {
       off();
     } else if (command == 't') {
-      if(temp == 'o') {
-        command = 'f';
-        off();
-      }
-      if(temp == 'f') {
-        command = 'o';
-        on();
-      }
+      toggle();
     }
   }
 }
 
 void on() {
   digitalWrite(ledPin, HIGH);
-  Serial.println("LED is on");
+  ledState = true;
+  Serial.println("LED is ON");
 }
 
 void off() {
   digitalWrite(ledPin, LOW);
-  Serial.println("LED is off");
+  ledState = false;
+  Serial.println("LED is OFF");
+}
+
+void toggle() {
+  if (ledState) {
+    off();
+  } else {
+    on();
+  }
 }
